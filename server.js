c775fdc3a1f4aa0cc68dd9cb58e5d58baf1437e3
REALcuba <angelskcuba@gmail.com>
2022-06-11 16:17:58 +0200

    body parse and post/getby id done

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const fs = require("fs");
 const app = express();
-const messagesData = require("./messagesData.json")
-
+const messagesData = require("./messagesData.json");
+const bodyParser = require("body-parser");
 app.use(cors());
 
 const welcomeMessage = {
@@ -12,7 +12,7 @@ const welcomeMessage = {
   text: "Welcome to CYF chat system!",
 };
 
-const file = "messagesData.json"
+const file = "messagesData.json";
 //This array is our "data store".
 //We will start with one message in the array.
 //Note: messages will be lost when Glitch restarts our server.
@@ -23,53 +23,47 @@ app.get("/", function (request, response) {
 });
 
 //Functions
-function getMessagesFromDatabase() {
-  const text = fs.readFileSync(file);
-  return JSON.parse(text);
-}
-// function saveMessageToDatabase(messages) {
-//   messages.id = messagesData.length;
-//   fs.writeFileSync(file, JSON.stringify(messages, null, 2));
-
+// function getMessagesFromDatabase() {
+//   const text =
+// fs.readFileSync(file);
+// return JSON.parse(text);
 // }
+function saveMessageToDatabase(message) {
+  messages.push(message);
+}
 const getMessageById = (req, res) => {
-  const messages = getMessagesFromDatabase();
-  const id = Number(req.params.id)
+  const id = Number(req.params.id);
 
-  const message = messages.find(message => id === message.id)
+  const message = messages.find((message) => id === message.id);
   console.log(message);
-  res.send(message)
-
+  res.send(message);
 };
 
 const postMessage = (req, res) => {
   let newMessage = req.body;
+  console.log("req body", req.body);
+  newMessage.id = messages.length;
+  saveMessageToDatabase(newMessage);
+  res.send(newMessage);
+};
 
-  newMessage.id = messagesData.length;
-  console.log("req body", newMessage.id);
-  messagesData.push(newMessage);
-  // saveMessageToDatabase(messages)
-  res.send(newMessage)
-
-}
-
-//WIREFRAME
+//MIDDLEWARE
 app.use(express.json());
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.get("/messages", (req, res) => {
-  res.send(messages)
-})
+  res.send(messages);
+});
 app.get("/messages/:id", getMessageById);
 
-app.post("/messages", postMessage)
+app.post("/messages", postMessage);
 
 app.delete("/messages/:id", (req, res) => {
-  const id = Number(req.params.id)
-  messages.splice(id, 1)
-  res.send(messages)
-})
-
+  const id = Number(req.params.id);
+  messages.splice(id, 1);
+  res.send(messages);
+});
 
 app.listen(3000, () => {
-  console.log("Listening on port 3000")
+  console.log("Listening on port 3000", "http://localhost:3000/");
 });
