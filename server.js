1974e9a048958f3593914e10eb2ddd027b83d4f9
REALcuba <angelskcuba@gmail.com>
2022-06-11 17:29:52 +0200

    exercise 3.1  done

--- server.js
+++ server.js
@@ -42,27 +42,42 @@ const getMessageById = (req, res) => {
 const postMessage = (req, res) => {
   let newMessage = req.body;
   console.log("req body", req.body);
+
+  if (!req.body.text || !req.body.from) {
+    return res.status(400).send("text/from file are empty");
+  }
   newMessage.id = messages.length;
   saveMessageToDatabase(newMessage);
   res.send(newMessage);
 };
 
+const searchMessage = (req, res) => {
+  const text = req.query.text.toLowerCase();
+  const findText = messages.filter((message) =>
+    message.text.toLowerCase().includes(text)
+  );
+
+  res.send(findText);
+};
 //MIDDLEWARE
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
 app.get("/messages", (req, res) => {
   res.send(messages);
 });
-app.get("/messages/:id", getMessageById);
 
 app.post("/messages", postMessage);
+app.get("/messages/search", searchMessage);
+app.get("/messages/:id", getMessageById);
 
 app.delete("/messages/:id", (req, res) => {
   const id = Number(req.params.id);
   messages.splice(id, 1);
   res.send(messages);
 });
+
 
 app.listen(3000, () => {
   console.log("Listening on port 3000", "http://localhost:3000/");
